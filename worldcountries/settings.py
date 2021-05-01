87b9f2f4f90add665a8f057ded933cec0674f863
skellsrs <45470999+skellsrs@users.noreply.github.com>
2021-05-02 08:55:49 +1000

    Changed secrets file to mysecrets

--- worldcountries/settings.py
+++ worldcountries/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'worldcountries',
         'USER': mysecrets.dbuser,
-        'PASS': mysecrets.dbpassword,
+        'PASSWORD': mysecrets.dbpassword,
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
