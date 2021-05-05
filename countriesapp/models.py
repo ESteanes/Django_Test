92156ec83dcac215eeaafbe66fb83a04a71c476e
skellsrs <45470999+skellsrs@users.noreply.github.com>
2021-05-05 21:00:30 +1000

    Got the test server up and running

--- countriesapp/models.py
+++ countriesapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
+class Countries(models.Model):
+    name = models.CharField(max_length=50,blank=False,default='')
+    captial = models.CharField(max_length=100,blank=False,default='')
+
+
 # Create your models here.
---
+++ countriesapp/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from countriesapp.models import Countries
+
+
+class CountriesAppSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Countries
+        fields = ('id','name','capital')
